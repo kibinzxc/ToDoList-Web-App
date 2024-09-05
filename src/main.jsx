d273fec1cd1c9c9f72f8f67db47743fb9333e5cb
Kevin <mamadaddyalexa@gmail.com>
2024-09-06 04:46:07 +0800

    test

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import Card from "./components/Card.jsx";
+import CreateTodo from "./components/CreateTodo.jsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <CreateTodo />
+  </StrictMode>
+);
