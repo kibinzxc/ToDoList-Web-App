9242d0b15b0e389b9a10420c1ea83a346b9ee258
Kevin <mamadaddyalexa@gmail.com>
2024-09-07 07:49:11 +0800

    Add task function

--- src/components/TaskColumn.jsx
+++ src/components/TaskColumn.jsx
@@ -2,14 +2,16 @@ import React from "react";
 import Todo from "../assets/direct-hit.png";
 
 import "./TaskColumn.css";
+import TaskCard from "./TaskCard";
 
-const TaskColumn = (props) => {
+const TaskColumn = ({ title, icon }) => {
   return (
     <section className='task_column'>
       <h2 className='task_column_heading'>
-        <img className='task_column_icon' src={props.icon} alt='' />
-        {props.title}
+        <img className='task_column_icon' src={icon} alt='' />
+        {title}
       </h2>
+      <TaskCard />
     </section>
   );
 };
--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -1,16 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./TaskForm.css";
 import Tag from "./Tag";
 
 const TaskForm = () => {
+  const [taskData, settaskData] = useState({
+    task: "",
+    status: "todo",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    settaskData((prev) => {
+      return {
+        ...prev,
+        [name]: value,
+      };
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(taskData);
+  };
+
+  // const [task, setTask] = useState("");
+  // const [status, setStatus] = useState("todo");
+
+  // const handleTaskChange = (e) => {
+  //   setTask(e.target.value);
+  // };
+
+  // const handleStatusChange = (e) => {
+  //   setStatus(e.target.value);
+  // };
+
   return (
     <header className='app_header'>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
           type='text'
+          name='task'
           className='task_input'
-          placeholder='Add a task'
+          placeholder='Enter your task'
+          onChange={handleChange}
         />
         <div className='task_form_bottom_line'>
           <div>
@@ -21,7 +55,11 @@ const TaskForm = () => {
           </div>
 
           <div>
-            <select className='task_status'>
+            <select
+              name='status'
+              className='task_status'
+              onChange={handleChange}
+            >
               <option value='todo'>To Do</option>
               <option value='doing'>In Progress</option>
               <option value='done'>Done</option>
