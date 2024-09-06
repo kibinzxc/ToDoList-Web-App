0216290de66ca76bd957e09431dd611c9cb2a493
Kevin <mamadaddyalexa@gmail.com>
2024-09-07 07:09:35 +0800

    Add Section Header

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import Card from "./components/Card.jsx";
-import CreateTodo from "./components/CreateTodo.jsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <CreateTodo />
+    <App />
   </StrictMode>
 );
