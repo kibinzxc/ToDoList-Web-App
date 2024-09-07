c83258f042baaed43a9c63921dde7f3b317c93b4
Kevin <mamadaddyalexa@gmail.com>
2024-09-07 08:53:41 +0800

    Finalization and polishes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import "./App.css";
 import TaskForm from "./components/TaskForm";
@@ -7,8 +7,14 @@ import todoIcon from "./assets/direct-hit.png";
 import inProgressIcon from "./assets/glowing-star.png";
 import doneIcon from "./assets/check-mark-button.png";
 
+const oldTasks = localStorage.getItem("tasks");
+console.log(oldTasks);
 const App = () => {
-  const [tasks, setTasks] = useState([]);
+  const [tasks, setTasks] = useState(JSON.parse(oldTasks) || []);
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
 
   const handleDelete = (taskIndex) => {
     const newTasks = tasks.filter(
